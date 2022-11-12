68d50aa4ab11409c29252c160ff9b5a924af4cfc
Briana Dodgen <brianadodgen@gmail.com>
2022-11-12 14:06:43 +0100

    create admin index page layout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\WorkerController;
 use Illuminate\Support\Facades\Route;
@@ -17,5 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [LoginController::class, 'index']);
+
+Route::get('/admin', [AdminController::class, 'index']);
+
 Route::get('/worker', [WorkerController::class, 'detail'])->name('worker.detail');
 Route::get('/logout', [WorkerController::class, 'logout']);
