d7c889449ef2c9b2b556c4ddad6f96bdd42e7cfc
Briana Dodgen <brianadodgen@gmail.com>
2022-11-12 10:54:20 +0100

    created main layout and  structure for worker detail page

---
+++ resources/views/worker/detail.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts/main')
+@section('content')
+
+<div class="worker-detail">
+
+  <h1>worker detail page goes here</h1>
+
+  <div class="graph-container"></div>
+  
+  <div class="exposure-record"></div>
+
+</div>
+
+@endsection
+
+{{-- DoD: Page loads when URL(VibeCzech/worker) is entered.
+Recommended HTML structure:
+-Overall container
+-Graph section
+-Record detail section --}}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\WorkerController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/worker', [WorkerController::class, 'detail'])->name('worker.detail');
