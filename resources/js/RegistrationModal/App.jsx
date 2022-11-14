78e6f71edd0c8f59fa244d095de8218fdb747e4e
Aidan <aidan.aitchison@gmail.com>
2022-11-14 13:32:01 +0100

    Cleaned up code, added comments to registration App.jsx

--- resources/js/RegistrationModal/App.jsx
+++ resources/js/RegistrationModal/App.jsx
@@ -5,37 +5,38 @@ export default function App() {
 
     const showModal = () => {
         const modal = document.getElementById("modal");
-        console.log(modal);
+        // This must be inside of the function because at the point it's called in the code, the return has not run yet and will return null
         modal.style.display = "block";
-        console.log(modal.style);
+        // Shows modal
     }
 
     const hideModal = (e) => {
         if (e.target == modal) {
             modal.style.display = "none";
+            // Hides modal if you click outside of the modal__content box
         }
     }
 
     return (
         <div>
-            <form>
+            <form> {/* Needs action */}
                 <label htmlFor='username'>Username</label>
-                <br />
+                <br /> {/* All breaks should eventually be replaced with styling */}
                 <input type='text' id='username' name='username' />
-                <br /><br />
+                <br /><br /> {/* All breaks should eventually be replaced with styling */}
 
                 <label htmlFor='password'>Password</label>
                 <br />
                 <input type='password' id='password' name='password' />
-                <br /><br />
+                <br /><br /> {/* All breaks should eventually be replaced with styling */}
 
                 <button id='login' name='login'>Login</button>
-                <br /><br />
+                <br /><br /> {/* All breaks should eventually be replaced with styling */}
 
                 <a href='#' id='register' onClick={showModal}>Register</a>
             </form>
 
-            <div id='modal' className='modal' onClick={hideModal} style={{ display: "none" }}>
+            <div id='modal' className='modal' onClick={hideModal}>
                 <div className='modal__content'>
                     <p>Modal</p>
                 </div>
--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -6,31 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>VibeCzech | Login</title>
-  <!-- <link rel="stylesheet" href="{{ asset('css/registration-modal.css')}}"> -->
-  <style>
-    .modal {
-        /* display: none; */
-        position: fixed;
-        z-index: 1;
-        left: 0;
-        top: 0;
-        width: 100%;
-        height: 100%;
-        overflow: auto;
-        background-color: rgb(0, 0, 0);
-        background-color: rgba(0, 0, 0, 0.4);
-    }
-
-    .modal__content {
-        background-color: #fefefe;
-        margin: 15% auto;
-        padding: 20px;
-        border: 1px solid #888;
-        width: 50%;
-        height: 20%;
-
-    }
-  </style>
+  <link rel="stylesheet" href="{{ asset('css/registration-modal.css')}}">
 </head>
 
 <body>
