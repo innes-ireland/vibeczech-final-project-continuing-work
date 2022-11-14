6c3e8b69ceefe47551e100cec45899a4f561d7e1
Aidan <aidan.aitchison@gmail.com>
2022-11-14 16:35:06 +0100

    Barring unforeseen variables, universal index is finished.

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -1,3 +1,5 @@
+@extends('layouts.main')
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,15 +8,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>VibeCzech | Login</title>
-  <link rel="stylesheet" href="{{ asset('css/index.css')}}">
+  <link rel="stylesheet" href="{{ asset('css/universal-index.css')}}">
   <link rel="stylesheet" href="{{ asset('css/registration-modal.css')}}">
 </head>
 
-<body>
+@section('content')
     <div id='root' class='container'>
         @viteReactRefresh
         @vite('resources/js/registration-modal.jsx')
     </div>
-</body>
+@endsection
 
 </html>
