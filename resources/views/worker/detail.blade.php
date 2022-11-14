59828a6ec609e021eeee161dd66c74078e974e3e
Aidan <aidan.aitchison@gmail.com>
2022-11-14 19:31:23 +0100

    Linked stylesheets in admin and worker blade files. Added if statements for admin modals. Minor modal.css tweaks.

--- resources/views/worker/detail.blade.php
+++ resources/views/worker/detail.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.main')
-@include('layouts.header')
 @include('layouts.footer')
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>VibeCzech | Login</title>
+  <link rel="stylesheet" href="{{ asset('css/universal-index.css')}}">
+
+</head>
 
 @section('content')
 
