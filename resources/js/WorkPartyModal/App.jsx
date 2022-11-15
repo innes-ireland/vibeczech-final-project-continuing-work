4d60579b72782619e7fc513415203dc8467be3aa
Briana Dodgen <brianadodgen@gmail.com>
2022-11-15 16:22:29 +0100

    login, logout, and register all working at bare minimum, some styling needed

--- resources/js/WorkPartyModal/App.jsx
+++ resources/js/WorkPartyModal/App.jsx
@@ -1,32 +1,37 @@
 import React from 'react';
+import axios from 'axios';
 
 export default function App() {
 
 
 
-    const showModal = () => {
-        const modal = document.getElementById("modal_work_party");
-        modal.style.display = "block";
+  const showModal = () => {
+    const modal = document.getElementById("modal_work_party");
+    modal.style.display = "block";
+  }
+
+  const hideModal = (e) => {
+    const modal = document.getElementById("modal_work_party");
+    if (e.target == modal) {
+      modal.style.display = "none";
     }
-
-    const hideModal = (e) => {
-        const modal = document.getElementById("modal_work_party");
-        if (e.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+  }
 
 
-    return (
-        <>
-            < a href='#' id='modalTrigger' onClick={showModal} > Create Work Party</a >
-            <div id='modal_work_party' class='modal' onClick={hideModal}>
-                <div className='modal__content'>
-                    <h1 >Work Party</h1>
-                </div>
-            </div>
-        </>
+  return (
+    <>
+      < a href='#' id='modalTrigger' onClick={showModal} > Create Work Party</a >
+      <div id='modal_work_party' class='modal' onClick={hideModal}>
+        <div className='modal__content'>
+          <h1 >Work Party</h1>
+        </div>
+      </div>
+      <span id='get-user' onClick={async function () {
+        const response = await axios('/api/user');
+        console.log(response);
+      }}>Get current user</span>
+    </>
 
 
-    )
+  )
 }
