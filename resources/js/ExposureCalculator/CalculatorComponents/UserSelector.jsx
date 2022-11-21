375d01e4d0ad5d0e92abc1b79070b20e4877d099
Briana Dodgen <brianadodgen@gmail.com>
2022-11-21 17:14:39 +0100

    redirected work party modal to the calculator

--- resources/js/ExposureCalculator/CalculatorComponents/UserSelector.jsx
+++ resources/js/ExposureCalculator/CalculatorComponents/UserSelector.jsx
@@ -1,49 +1,49 @@
 export default function userSelector({ user, setUser }) {
 
-    const handleUserChange = (e) => {
-        setUser(e.target.value)
+  const handleUserChange = (e) => {
+    setUser(e.target.value)
 
-    }
-
-
-
-    const users = [{
-        id: 1,
-        name: 'Innes',
-
-    },
-    {
-        id: 2,
-        name: 'Aidan',
-
-    },
-    {
-        id: 3,
-        name: 'Briana',
-
-    },
-    {
-        id: 4,
-        name: 'Sabina',
-    }
+  }
 
 
 
-    ] // this is hardcoded to represent an array of objects retrieved from database
+  const users = [{
+    id: 1,
+    name: 'Innes',
+
+  },
+  {
+    id: 2,
+    name: 'Aidan',
+
+  },
+  {
+    id: 3,
+    name: 'Briana',
+
+  },
+  {
+    id: 4,
+    name: 'Sabina',
+  }
+
+
+
+  ] // this is hardcoded to represent an array of objects retrieved from database
 
 
 
 
 
-    return (
-        <select onChange={handleUserChange}>
-            <option value='null'> </option>
-            {
-                users.map((user) => {
-                    return <option value={user.name}> {user.name}</option>
+  return (
+    <select onChange={handleUserChange}>
+      <option value='null'> </option>
+      {
+        users((user) => {
+          return <option value={user.name}> {user.name}</option>
 
-                })}
-        </select>
+        })}
+    </select>
 
-    );
+  );
 }
--- resources/js/WorkPartyModal/App.jsx
+++ resources/js/WorkPartyModal/App.jsx
@@ -5,159 +5,162 @@ import { useState, useEffect } from 'react';
 
 export default function App() {
 
-    const [modalVisible, setModalVisible] = useState(false)
-    const [workers, setWorkers] = useState([]);
-    const [party, setParty] = useState([]);
-    const [values, setValues] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false)
+  const [workers, setWorkers] = useState([]);
+  const [party, setParty] = useState([]);
+  const [values, setValues] = useState([]);
 
 
 
-    //-------this section displays and hides the modal----------
-    const showModal = () => {
-        setModalVisible(true)
+  //-------this section displays and hides the modal----------
+  const showModal = () => {
+    setModalVisible(true)
+  }
+
+  const hideModal = (e) => {
+    const modal = document.getElementById("modal_work_party");
+    const button = document.getElementById("button_add_party");
+    if (e.target == modal) {
+      setModalVisible(false)
+    } else if (e.target == button) {
+      setModalVisible(false)
     }
+  }
+  //-------this section displays and hides the modal----------
 
-    const hideModal = (e) => {
-        const modal = document.getElementById("modal_work_party");
-        const button = document.getElementById("button_add_party");
-        if (e.target == modal) {
-            setModalVisible(false)
-        } else if (e.target == button) {
-            setModalVisible(false)
-        }
+  //---------fetch to get data to display workers------------
+  const loadWorkers = async () => {
+
+    // Getting data with axios
+    try {
+      const response = await axios.get('/api/work-party');
+      setWorkers(response.data);
+    } catch (err) {
+      console.log(err);
     }
-    //-------this section displays and hides the modal----------
+  }
 
-    //---------fetch to get data to display workers------------
-    const loadWorkers = async () => {
-
-        // Getting data with axios
-        try {
-            const response = await axios.get('/api/work-party');
-            setWorkers(response.data);
-        } catch (err) {
-            console.log(err);
-        }
-    }
-
-    useEffect(() => {
-        loadWorkers();
-    }, [])
-    //---------fetch to get data to display workers------------
+  useEffect(() => {
+    loadWorkers();
+  }, [])
+  //---------fetch to get data to display workers------------
 
 
-    //-------add and remove function-----------
-    const addParty = (temp) => {
+  //-------add and remove function-----------
+  const addParty = (temp) => {
 
-        const newParty = {
-            id: temp.id,
-            name: temp.first_name,
-            last_name: temp.last_name,
-        };
-
-        let new_arr = party;
-
-        setParty(new_arr.concat(newParty));
-        const new_workers = workers.filter((worker) => worker.id !== newParty.id);
-
-        setWorkers(new_workers)
+    const newParty = {
+      id: temp.id,
+      name: temp.first_name,
+      last_name: temp.last_name,
     };
 
-    const deleteParty = (id) => {
-        const newParty = party.filter((temp2) => temp2.id !== id);
+    let new_arr = party;
 
-        setParty(newParty);
-    };
-    //-------add and remove function-----------
+    setParty(new_arr.concat(newParty));
+    const new_workers = workers.filter((worker) => worker.id !== newParty.id);
+
+    setWorkers(new_workers)
+  };
+
+  const deleteParty = (id) => {
+    const newParty = party.filter((temp2) => temp2.id !== id);
+
+    setParty(newParty);
+  };
+  //-------add and remove function-----------
 
 
 
-    const handleChange = (event) => {
-        setValues(previous_values => {
-            return ({
-                ...previous_values,
-                [event.target.name]: event.target.value
-            });
-        });
+  const handleChange = (event) => {
+    setValues(previous_values => {
+      return ({
+        ...previous_values,
+        [event.target.name]: event.target.value
+      });
+    });
+  }
+
+
+  //---------POST request---------------
+  const handleSubmit = async (event) => {
+
+    event.preventDefault();
+
+    const array = [party, values]
+    // with axios
+    const response = await fetch('/api/new-work-party', {
+      method: 'POST',
+      body: JSON.stringify(array),
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json',
+        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+      }
+    });
+
+    // parse the response as JSON
+    const response_data = await response.json();
+
+    // if the response code is not 2xx (success)
+    if (Math.floor(response.status / 100) !== 2) {
+      switch (response.status) {
+        case 422:
+          // handle validation errors here
+          console.log('VALIDATION FAILED:', response_data.errors);
+          break;
+        default:
+          console.log('UNKNOWN ERROR', response_data);
+          break;
+      }
     }
 
 
-    //---------POST request---------------
-    const handleSubmit = async (event) => {
+    window.location.replace('/exposure-calculator');
+  }
+  //---------POST request---------------
 
-        event.preventDefault();
+  return (
+    <>
+      < a href='#' id='modalTrigger' onClick={showModal} > Create Work Party</a >
+      {
+        modalVisible ?
+          <div id='modal_work_party' className='modal' onClick={hideModal}>
+            <div className='modal__content'>
+              <h1 >Work Party</h1>
+              <div>
+                <div>Short job description: <input type="text" name="job" value={values.job} onChange={handleChange} /></div>
+                <div>Date: <input type="date" name="work_date" value={values.work_date} onChange={handleChange}></input></div>
+                <ul>
+                  {party.map(user => {
+                    return <li key={user.id}>
+                      <p>{user.name} {user.last_name}</p>
+                      <button onClick={() => deleteParty(user.id)}>&times;</button>
+                    </li>
+                  })}
+                </ul>
 
-        const array = [party, values]
-        // with axios
-        const response = await fetch('/api/new-work-party', {
-            method: 'POST',
-            body: JSON.stringify(array),
-            headers: {
-                'Accept': 'application/json',
-                'Content-type': 'application/json',
-                'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-            }
-        });
-
-        // parse the response as JSON
-        const response_data = await response.json();
-
-        // if the response code is not 2xx (success)
-        if (Math.floor(response.status / 100) !== 2) {
-            switch (response.status) {
-                case 422:
-                    // handle validation errors here
-                    console.log('VALIDATION FAILED:', response_data.errors);
-                    break;
-                default:
-                    console.log('UNKNOWN ERROR', response_data);
-                    break;
-            }
-        }
-    }
-    //---------POST request---------------
-
-    return (
-        <>
-            < a href='#' id='modalTrigger' onClick={showModal} > Create Work Party</a >
-            {
-                modalVisible ?
-                    <div id='modal_work_party' className='modal' onClick={hideModal}>
-                        <div className='modal__content'>
-                            <h1 >Work Party</h1>
-                            <div>
-                                <div>Short job description: <input type="text" name="job" value={values.job} onChange={handleChange} /></div>
-                                <div>Date: <input type="date" name="work_date" value={values.work_date} onChange={handleChange}></input></div>
-                                <ul>
-                                    {party.map(user => {
-                                        return <li key={user.id}>
-                                            <p>{user.name} {user.last_name}</p>
-                                            <button onClick={() => deleteParty(user.id)}>&times;</button>
-                                        </li>
-                                    })}
-                                </ul>
-
-                                {/* below is POST request to send data to database */}
-                                <form action="/api/new-work-party" method="post" >
-                                    <button id='button_add_party'
-                                        onClick={handleSubmit}
-                                    >Create Work Party</button>
-                                </form>
-                            </div>
-                            {
-                                workers.map(worker => {
-                                    return <div className='users_display' key={worker.id}>
-                                        <p>{worker.first_name} {worker.last_name}</p>
-                                        <button onClick={() => addParty(worker)
-                                        }>Add</button>
-                                    </div>
-                                })
-                            }
-                        </div>
-                    </div>
-                    :
-                    <></>
-            }
-        </>
-    )
+                {/* below is POST request to send data to database */}
+                <form action="/api/new-work-party" method="post" >
+                  <button id='button_add_party'
+                    onClick={handleSubmit}
+                  >Create Work Party</button>
+                </form>
+              </div>
+              {
+                workers.map(worker => {
+                  return <div className='users_display' key={worker.id}>
+                    <p>{worker.first_name} {worker.last_name}</p>
+                    <button onClick={() => addParty(worker)
+                    }>Add</button>
+                  </div>
+                })
+              }
+            </div>
+          </div>
+          :
+          <></>
+      }
+    </>
+  )
 }
