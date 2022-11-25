a9e7514562bd41c4ad92392e53341d1bc4bfd3b1
Innes Ireland <innesmireland@gmail.com>
2022-11-25 09:09:02 +0000

    documentation view in progress

---
+++ resources/views/Documentation/documentation.blade.php
@@ -0,0 +1,19 @@
+<div class = "documentation_container">
+    <div class = "eu_docs">
+    <h2> For EU Employers and Workers </h2>
+    <ul>
+    <li><a href="https://osha.europa.eu/en/legislation/directives/19"> European Agency for health and safety at work guidelines </a></li>
+    <li><a href="https://resource.isvr.soton.ac.uk/HRV/VIBGUIDE/HAV%20Good%20practice%20Guide%20V7.7%20English%20260506.pdf"> EU good practice guide </a></li>
+    </ul>
+    </div>
+    <div class="uk_docs">
+    <h2> for UK Employers and Workers </h2>
+    <ul>
+    <li><a href= "https://www.hse.gov.uk/vibration/hav/indg296.htm"> HSE guidance on Hand Arm Vibrations for employees </a></li>
+    <li><a href= "https://www.hse.gov.uk/pubns/indg175.htm"> HSE guidance on Hand Arm Vibrations for employers </a></li>
+    </ul>
+    <div class="us_docs">
+        <ul>
+            <li> <a href="https://resource.isvr.soton.ac.uk/HRV/VIBGUIDE/HAV%20Good%20practice%20Guide%20V7.7%20English%20260506.pdf"> NIOSH good practice guide </a>
+    </div>
+
