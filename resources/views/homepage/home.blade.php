8077ca0b85a0319e9fc5ccd6f0fbbf2fa332009a
Sabina Tokadzhieva <jenskikomar@gmail.com>
2022-11-25 09:46:45 +0100

    final touches on homepage

--- resources/views/homepage/home.blade.php
+++ resources/views/homepage/home.blade.php
@@ -43,11 +43,13 @@
   <div class="container">
     <div class="center">
       <button class="btn">
+        <a href="/login">
         <svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
           <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
           <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
         </svg>
-        <span><a href="/login">LOGIN/REGISTER</a></span>
+        <span>LOGIN/REGISTER</span>
+        </a>
       </button>
     </div>
   </div>
@@ -58,6 +60,8 @@
 
     </body>
 </html>
+
+{{-- window.location.replace('/login') --}}
 
     
 
